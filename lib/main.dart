eda8c2ddc1887c73fe6b5df555270a655fb5ad13
Kathiravan <mkathiravanmca@gmail.com>
2021-03-29 12:56:36 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_transform_widget/screens/splash_screen.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget
+{
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Splash(),
+    );
+  }
+  
+}
